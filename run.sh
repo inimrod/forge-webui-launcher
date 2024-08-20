c5c0d3827bd05c3e849dc7086bf5e4b042f052f6
iNimrod <brodlefleur@gmail.com>
2024-08-20 23:40:27 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 ##### SET FOLLOWING VARS #####
 SERVER_ADDR="0.0.0.0"
 SERVER_PORT=7867
+CLI_ARGS="--cuda-malloc"
 clone_dir="forge"
 venv_dir="venv"
 ###### END OF VARIABLES ######
@@ -15,11 +16,10 @@ cd $SCRIPT_DIR
 # check if install dir already exists
 if [ ! -d "$clone_dir" ]; then
     git clone https://github.com/lllyasviel/stable-diffusion-webui-forge.git forge
-    # cd forge
-    # sed -i -e "s/server_name='0.0.0.0'/ /" gradio_app.py # need to remove this so we can set via env var below
 fi
 
 export GRADIO_SERVER_NAME=$SERVER_ADDR
 export GRADIO_SERVER_PORT=$SERVER_PORT
+export COMMANDLINE_ARGS="$CLI_ARGS"
 
 source $clone_dir/webui.sh
